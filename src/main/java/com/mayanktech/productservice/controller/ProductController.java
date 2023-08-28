eaedb23a7268242bb4de1a586bd718608cd84fc0
Mayank <mayankyadav1503@gmail.com>
2023-08-28 23:41:55 +0530

    get all products

--- src/main/java/com/mayanktech/productservice/controller/ProductController.java
+++ src/main/java/com/mayanktech/productservice/controller/ProductController.java
@@ -1,6 +1,9 @@
 package com.mayanktech.productservice.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +11,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mayanktech.productservice.dto.ProductRequest;
+import com.mayanktech.productservice.dto.ProductResponse;
 import com.mayanktech.productservice.service.ProductService;
 
 import lombok.RequiredArgsConstructor;
@@ -23,5 +27,11 @@ public class ProductController {
 	@ResponseStatus(HttpStatus.CREATED)
 	public void createProduct(@RequestBody ProductRequest productRequest) {
 		productService.createProduct(productRequest);
+	}
+	
+	@GetMapping
+	@ResponseStatus(HttpStatus.OK)
+	public List<ProductResponse> getAllProducts(){
+		return productService.getAllProducts();
 	}
 }
